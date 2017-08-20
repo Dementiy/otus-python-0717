c956b7cb1ad5a8f396359318ed64685eebd35673
dementiy <Dementiy@yandex.ru>
2017-08-20 16:11:41 +0300

    Добавлен кастомный менеджер

--- homework06/hasker/qa/models.py
+++ homework06/hasker/qa/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.utils.encoding import python_2_unicode_compatible
+from django.shortcuts import reverse
 
 
 class TimestampedModel(models.Model):
@@ -24,6 +25,12 @@ class Tag(models.Model):
         return self.name
 
 
+class QuestionManager(models.Manager):
+
+    def trending(self):
+        return self.order_by('-votes', '-created_at')[:5]
+
+
 @python_2_unicode_compatible
 class Question(TimestampedModel):
     slug = models.SlugField(max_length=254)
@@ -33,11 +40,17 @@ class Question(TimestampedModel):
     votes = models.IntegerField(default=0)
     answered = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name='questions')
+    objects = QuestionManager()
 
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = slugify(self.title)
         super(Question, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("qa:question", kwargs={
+            "slug": self.slug,
+        })
 
     def __str__(self):
         return self.title
