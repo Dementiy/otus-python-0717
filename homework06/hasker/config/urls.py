823b2b3330eec22a9f7071c0fe399829154b1cb0
dementiy <Dementiy@yandex.ru>
2017-08-20 16:04:48 +0300

    Медиа только в режиме отладки

--- homework06/hasker/config/urls.py
+++ homework06/hasker/config/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^', include('qa.urls', namespace='qa')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
