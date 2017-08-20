6d15942f4d5a4e29b8a5db4f3c01a2b3f723f302
dementiy <Dementiy@yandex.ru>
2017-08-20 12:52:00 +0300

    Добавлен вьювер для регистрации пользователя

--- homework06/hasker/accounts/views.py
+++ homework06/hasker/accounts/views.py
@@ -3,4 +3,26 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import SignUpForm
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect("")
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.avatar = form.cleaned_data.get("avatar") or "accounts/avatars/avatar.png"
+            user.save()
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('')
+    else:
+        form = SignUpForm()
+    return render(request, "accounts/signup.html", {
+        "form": form
+    })
+
