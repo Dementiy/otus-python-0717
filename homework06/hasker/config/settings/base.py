0c493310931db42b197f976b40a26d48d6a33292
dementiy <Dementiy@yandex.ru>
2017-08-20 16:13:21 +0300

    Список популярных вопросов добавлен в контекст шаблонов

--- homework06/hasker/config/settings/base.py
+++ homework06/hasker/config/settings/base.py
@@ -51,6 +51,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'qa.context_processors.trending',
             ],
         },
     },
