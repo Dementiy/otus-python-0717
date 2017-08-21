e0b70c0910f26980d379a348d870d9a553817e83
dementiy <Dementiy@yandex.ru>
2017-08-21 19:04:30 +0300

    Обновлены локальные настройки проекта

--- homework06/hasker/config/settings/local.py
+++ homework06/hasker/config/settings/local.py
@@ -9,5 +9,13 @@ DATABASES = {
     }
 }
 
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=True, cast=bool)
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = root('media')
