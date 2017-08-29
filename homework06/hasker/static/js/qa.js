0269f2c69a8bebeaf609c3771ab41c536ca66eb0
dementiy <Dementiy@yandex.ru>
2017-08-29 23:49:28 +0300

    Рефакторинг JS

--- homework06/hasker/static/js/qa.js
+++ homework06/hasker/static/js/qa.js
@@ -1,65 +1,60 @@
-function mark(answerId) {
-    $.ajax({
-        url: "/answer/" + answerId + "/mark",
-        type: "post",
-        dataType: "json",
-        success: function(data) {
-            if (data.mark) {
-                var el = $("#answer" + answerId);
-                if (el.attr("class").toString() == "ui right floated grey label") {
-                    el.removeClass("ui right floated grey label");
-                    el.addClass("ui right floated green label");
-                } else {
-                    el.removeClass("ui right floated green label");
-                    el.addClass("ui right floated grey label");
+!function ($) {
+    var Vote = function (element) {
+        this.$element = $(element);
+    }
+
+    Vote.prototype.update = function(url, value) {
+        var $i = this.$element.find('i');
+        var $span = $i.find('span');
+        $.ajax({
+            url: url,
+            type: "post",
+            data: {
+                "value": value
+            },
+            dataType: "json",
+            success: function(data) {
+                $span.text(data.votes);
+            }
+        })
+    }
+
+    $.fn.vote = function(url, value) {
+        return this.each(function () {
+            var $this = $(this),
+                data = $this.data('jpl.vote');
+
+            if (!data) $this.data('jpl.vote', (data = new Vote(this)));
+            if (typeof value == 'number') data.update(url, value);
+        })
+    };
+
+    $(document).on('click', 'a.vote', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var target = $($this.data('target'));
+        var href = $this.attr('href');
+        var value = $this.data('value');
+        target.vote(href, value);
+    });
+
+    $(document).on('click', 'a.mark', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var url = $this.attr('href');
+        $.ajax({
+            url: url,
+            type: "post",
+            dataType: "json",
+            success: function(data) {
+                if (data.mark) {
+                    if ($this.hasClass("green")) {
+                        $this.removeClass('green').addClass('grey');
+                    } else {
+                        $this.removeClass('grey').addClass('green');
+                    }
                 }
             }
-        }
-    })
-}
-
-function questionVote(qId, value) {
-    $.ajax({
-        url: "/question/" + qId + "/vote",
-        type: "post",
-        data: {
-            "value": value
-        },
-        dataType: "json",
-        success: function(data) {
-            var question_vote = $("#question_vote");
-            question_vote.text(data.votes);
-        },
-   })
-}
-
-function questionVoteUp(qId) {
-    questionVote(qId, 1);
-}
-
-function questionVoteDown(qId) {
-    questionVote(qId, -1);
-}
-
-function answerVote(id, value) {
-    $.ajax({
-        url: "/answer/" + id + "/vote",
-        type: "post",
-        data: {
-            "value": value
-        },
-        dataType: "json",
-        success: function(data) {
-            var answer_vote = $("#answer_vote" + id);
-            answer_vote.text(data.votes);
-        },
-   })
-}
-
-function answerVoteDown(aId) {
-    answerVote(aId, -1);
-}
-
-function answerVoteUp(aId) {
-    answerVote(aId, 1);
-}
+        })
+    });
+}(window.jQuery);
